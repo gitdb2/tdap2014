7208f40b4c4e01cca11deaefb6b3fe5f463ec7fd
Rodrigo <rodrigodemi@gmail.com>
2014-03-01 14:15:31 -0200

    agrego manejadores de usuarios

---
+++ TallerAplicaciones/Business/ManejadorUsuario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using uy.edu.ort.taller.aplicaciones.interfaces;
+using uy.edu.ort.taller.aplicaciones.dominio;
+
+namespace uy.edu.ort.taller.aplicaciones.negocio
+{
+    public class ManejadorUsuario : IUsuario
+    {
+        #region singleton
+        private static ManejadorUsuario instance = new ManejadorUsuario();
+
+        private ManejadorUsuario() { }
+
+        public static ManejadorUsuario GetInstance()
+        {
+            return instance;
+        }
+        #endregion
+
+        public void AltaUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModificarUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BajaUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> ListarUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario BuscarUsuario(int usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TallerAplicaciones/Interfaces/Interface1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using uy.edu.ort.taller.aplicaciones.dominio;
+
+namespace uy.edu.ort.taller.aplicaciones.interfaces
+{
+    public interface IUsuario
+    {
+         void AltaUsuario(Usuario usuario);
+         void ModificarUsuario(Usuario usuario);
+         void BajaUsuario(Usuario usuario);
+
+         List<Usuario> ListarUsuarios();
+         Usuario BuscarUsuario(int usuarioId);
+
+         bool Login(string login, string password);
+    }
+}
