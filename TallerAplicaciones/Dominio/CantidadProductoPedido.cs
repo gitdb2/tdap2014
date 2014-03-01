b9268859a28e23041bfb4479ca651d61825106d7
Rodrigo <rodrigodemi@gmail.com>
2014-03-01 12:46:54 -0200

    pedido

--- TallerAplicaciones/Dominio/CantidadProductoPedido.cs
+++ TallerAplicaciones/Dominio/CantidadProductoPedido.cs
@@ -7,6 +7,7 @@ namespace uy.edu.ort.taller.aplicaciones.dominio
 {
     public class CantidadProductoPedido
     {
+        public int CantidadProductoPedidoId { get; set; }
         public int Cantidad { get; set; }
         public Producto Producto { get; set; }
         public Pedido Pedido { get; set; }
--- TallerAplicaciones/Dominio/Pedido.cs
+++ TallerAplicaciones/Dominio/Pedido.cs
@@ -7,5 +7,12 @@ namespace uy.edu.ort.taller.aplicaciones.dominio
 {
     public class Pedido
     {
+        public int PedidoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public bool Aprobado { get; set; }
+        public List<CantidadProductoPedido> CantidadProductoPedidoList { get; set; }
+        public Distribuidor Distribuidor { get; set; }
+        public EjecutivoDeCuenta Ejecutivo { get; set; }
     }
 }
