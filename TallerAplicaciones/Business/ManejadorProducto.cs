37885e7ca52216cefe6e7f33ee933cea735cd00a
mauricio <karbunkle@gmail.com>
2014-03-01 20:19:11 -0200

    manejador e interfaz Producto

---
+++ TallerAplicaciones/Business/ManejadorProducto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using uy.edu.ort.taller.aplicaciones.interfaces;
+
+namespace uy.edu.ort.taller.aplicaciones.negocio
+{
+    public class ManejadorProducto : IProducto
+    {
+        #region singleton
+        private static ManejadorProducto instance = new ManejadorProducto();
+
+        private ManejadorProducto() { }
+
+        public static ManejadorProducto GetInstance()
+        {
+            return instance;
+        }
+        #endregion
+
+        public void AltaProducto(dominio.Producto producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BajaProducto(int idProducto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModificarProducto(dominio.Producto producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IProducto> ListarProductos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AgregarImagenProducto(int idProducto, dominio.Archivo imagen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AgregarVideoProducto(int idProducto, dominio.Archivo video)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TallerAplicaciones/Interfaces/IProducto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using uy.edu.ort.taller.aplicaciones.dominio;
+
+namespace uy.edu.ort.taller.aplicaciones.interfaces
+{
+    public interface IProducto
+    {
+
+        void AltaProducto(Producto producto);
+
+        void BajaProducto(int idProducto);
+
+        void ModificarProducto(Producto producto);
+
+        List<IProducto> ListarProductos();
+
+        void AgregarImagenProducto(int idProducto, Archivo imagen);
+
+        void AgregarVideoProducto(int idProducto, Archivo video);
+
+    }
+}
