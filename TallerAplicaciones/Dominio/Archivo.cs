f5116917fbeb755ebd424cf0bf9d46b8f098738b
mauricio <karbunkle@gmail.com>
2014-03-01 11:55:39 -0200

    archivo

---
+++ TallerAplicaciones/Dominio/Archivo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uy.edu.ort.taller.aplicaciones.dominio
+{
+    public abstract class Archivo
+    {
+
+        public int ArchivoID { get; set; }
+
+        public string Nombre { get; set; }
+        public string URL { get; set; }
+        public long Tamanio { get; set; }
+
+    }
+}
