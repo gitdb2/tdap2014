aef1131522c2b134ab28906c44227279b0995e58
mauricio <karbunkle@gmail.com>
2014-03-01 19:24:09 -0200

    agregue dbsets en persistencia

--- TallerAplicaciones/Business/Persistencia.cs
+++ TallerAplicaciones/Business/Persistencia.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Data.Entity;
 using uy.edu.ort.taller.aplicaciones.dominio;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace uy.edu.ort.taller.aplicaciones.negocio
 {
@@ -18,6 +19,21 @@ namespace uy.edu.ort.taller.aplicaciones.negocio
 
         public DbSet<PerfilUsuario> PerfilesUsuario { get; set; }
 
+        public DbSet<EmpresaDistribuidora> Empresas { get; set; }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        public DbSet<Atributo> Atributos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+       
+    }
+
+    public class PersistenciaInitializer : IDatabaseInitializer<Persistencia>
+    {
         public void InitializeDatabase(Persistencia context)
         {
             if (context.Database.Exists() && !context.Database.CompatibleWithModel(false))
@@ -28,10 +44,11 @@ namespace uy.edu.ort.taller.aplicaciones.negocio
             if (!context.Database.Exists())
             {
                 context.Database.Create();
-                context.Database.ExecuteSqlCommand("ALTER TABLE \"Usuario\" ADD CONSTRAINT UniqueLogin UNIQUE (login)");
+                context.Database.ExecuteSqlCommand("ALTER TABLE \"Usuario\" ADD CONSTRAINT UniqueLoginUsuario UNIQUE (login)");
+                context.Database.ExecuteSqlCommand("ALTER TABLE \"PerfilUsuario\" ADD CONSTRAINT UniqueLoginPerfilUsuario UNIQUE (login)");
             }
         }
-
-
     }
+
+
 }
