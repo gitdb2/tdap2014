688dc6161a717470fd68437530ed30e5a3a70601
Rodrigo <rodrigodemi@gmail.com>
2014-03-01 12:32:22 -0200

    pedido y cantidad productopedido

---
+++ TallerAplicaciones/Dominio/CantidadProductoPedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uy.edu.ort.taller.aplicaciones.dominio
+{
+    public class CantidadProductoPedido
+    {
+        public int Cantidad { get; set; }
+        public Producto Producto { get; set; }
+        public Pedido Pedido { get; set; }
+    }
+}
---
+++ TallerAplicaciones/Dominio/Pedido.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uy.edu.ort.taller.aplicaciones.dominio
+{
+    public class Pedido
+    {
+    }
+}
