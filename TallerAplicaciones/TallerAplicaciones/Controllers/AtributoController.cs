02eb4ce2871c44ca0c9287524ccb7eb51d3228d3
kur <kurkovani@gmail.com>
2014-03-03 20:20:18 -0200

    Formulario de atributos

---
+++ TallerAplicaciones/TallerAplicaciones/Controllers/AtributoController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TallerAplicaciones.Controllers
+{
+    public class AtributoController : Controller
+    {
+        //
+        // GET: /Atributo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Atributo/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Atributo/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Atributo/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Atributo/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Atributo/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Atributo/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Atributo/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ TallerAplicaciones/TallerAplicaciones/Models/AtributoModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TallerAplicaciones.Models
+{
+    public class AtributoModel
+    {
+
+        [Required]
+        [Display(Name = "Nombre de Atributo")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Atributo tipo combo")]
+        public bool DataCombo { get; set; }
+
+        [Required]
+        [Display(Name = "Valores")]
+        public List<String> Valores { get; set; }
+
+    }
+}
