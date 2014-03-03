75693b362e2fd403f4f3dc86677efbb0e1944a5d
mauricio <karbunkle@gmail.com>
2014-03-03 21:04:18 -0200

    antes de agregar la segunda pagina

---
+++ TallerAplicaciones/DistribuidoresApp/Views/DataDistribuidor.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using DistribuidoresApp.Temp;
+
+namespace DistribuidoresApp.Views
+{
+    public partial class DataDistribuidor : Page
+    {
+        public List<PedidoFake> Pedidos;
+
+        public DataDistribuidor()
+        {
+            InitializeComponent();
+            Pedidos = new List<PedidoFake>
+            {
+                new PedidoFake() {PedidoFakeId = 1, Descripcion = "Pedido1", Aprobado = true},
+                new PedidoFake() {PedidoFakeId = 2, Descripcion = "Pedido2", Aprobado = false},
+                new PedidoFake() {PedidoFakeId = 3, Descripcion = "Pedido3", Aprobado = false},
+                new PedidoFake() {PedidoFakeId = 4, Descripcion = "Pedido4", Aprobado = true}
+            };
+            ListBoxPedidos.ItemsSource = Pedidos;
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+    }
+}
