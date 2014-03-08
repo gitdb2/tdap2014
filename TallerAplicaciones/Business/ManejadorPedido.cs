27eb39c7581b4decb4ab12a4a50cb14b5230b1a3
Rodrigo <rodrigodemi@gmail.com>
2014-03-08 19:46:20 -0200

    listado andando

--- TallerAplicaciones/Business/ManejadorPedido.cs
+++ TallerAplicaciones/Business/ManejadorPedido.cs
@@ -26,43 +26,53 @@ namespace uy.edu.ort.taller.aplicaciones.negocio
 
         public List<Pedido> ListarPedidos()
         {
-           
+
             using (var db = new Persistencia())
             {
 
-              
+
 
 
                 return db.Pedidos
                     .Include(p => p.Ejecutivo)
                     .Include(p => p.Ejecutivo.Usuario)
                     .Include(p => p.Distribuidor)
+                    .Include(p => p.Distribuidor.Usuario)
                     .Include(p => p.Distribuidor.Empresa)
                     .Include(p => p.CantidadProductoPedidoList)
+                    
                     //.Include("CantidadProductoPedidoList")
                     //.Include("Distribuidor")
-                  
+
                     //.Include("Empresa")
-                   
-                     
+
+
                     .ToList();
             }
-      
+
         }
-     
-        public void Alta(Pedido pedido, int idDistribuidor, int idEjecutivo, 
+
+        public void Alta(Pedido pedido, int idDistribuidor, int idEjecutivo,
                                      List<int> productos, List<int> cantidades)
         {
             using (var db = new Persistencia())
             {
-                var distrib = ManejadorPerfilUsuario.GetInstance().FindDistribuidor(idDistribuidor);
-                db.PerfilesUsuario.Attach(distrib);
+                //var distrib = ManejadorPerfilUsuario.GetInstance().FindDistribuidor(idDistribuidor);
+                //db.PerfilesUsuario.Attach(distrib);
 
-                var ejecutivo = ManejadorPerfilUsuario.GetInstance().FindEjecutivo(idEjecutivo);
-                db.PerfilesUsuario.Attach(ejecutivo);
+                //var ejecutivo = ManejadorPerfilUsuario.GetInstance().FindEjecutivo(idEjecutivo);
+                //  db.PerfilesUsuario.Attach(ejecutivo);
+                pedido.Distribuidor = db.PerfilesUsuario.OfType<Distribuidor>()
+                    .Include(p => p.Empresa)
+                    .Include(p => p.Usuario)
+                    .SingleOrDefault(p => p.PerfilUsuarioID == idDistribuidor);
+                
 
-                pedido.Ejecutivo    = ejecutivo;
-                pedido.Distribuidor = distrib;
+                pedido.Ejecutivo = db.PerfilesUsuario.OfType<EjecutivoDeCuenta>()
+                    .Include(p => p.Usuario)
+                    .SingleOrDefault(p => p.PerfilUsuarioID == idEjecutivo);
+
+                
                 pedido.CantidadProductoPedidoList = new List<CantidadProductoPedido>();
 
                 for (int i = 0; i < productos.Count; i++)
@@ -81,6 +91,8 @@ namespace uy.edu.ort.taller.aplicaciones.negocio
                     };
                     pedido.CantidadProductoPedidoList.Add(cantidadProducto);
                 }
+
+                db.Pedidos.Add(pedido);
                 db.SaveChanges();
             }
 
